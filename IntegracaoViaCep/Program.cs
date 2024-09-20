1785158061da6898cc6544ab9c7d294f65a1b334
Daniel Paiva <danpaiva@live.com>
2024-09-20 09:39:12 -0300

    feat: first commit

---
+++ IntegracaoViaCep/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using static System.Console;
+
+WriteLine("\nDigite o seu CEP: ");
+var cep = ReadLine();
+
+WriteLine("\nRealizando aquisição na API... ");
+var enderecoUrl = $@"https://viacep.com.br/ws/{cep}/json/";
+
+var cliente = new HttpClient();
+
+try
+{
+  HttpResponseMessage? resposta = await cliente.GetAsync(enderecoUrl);
+  resposta.EnsureSuccessStatusCode();
+
+  string respostaApiString = await resposta.Content.ReadAsStringAsync();
+  WriteLine("Imprimindo todo o json: " + respostaApiString);
+
+  WriteLine("Transformando a minha string em um objeto C# :");
+  // Deserializando a resposta JSON
+  var objetoEndereco = JsonSerializer.Deserialize<Endereco>(respostaApiString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+  WriteLine("\nCEP: " + objetoEndereco.Cep);
+  WriteLine("Rua: " + objetoEndereco.Logradouro);
+  WriteLine("Estado: " + objetoEndereco.Estado);
+}
+catch (SystemException e)
+{
+  WriteLine("\nErro da Api: " + e.Message);
+}
